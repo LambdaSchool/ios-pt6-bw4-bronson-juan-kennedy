9201f786aa880217b5c1d1a8da78e06162990567
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-10-14 18:08:47 -0700

    Starter project, storyboard and basic files

---
+++ Home Mortgage Calculator/Home Mortgage Calculator/Mortgage.m
@@ -0,0 +1,12 @@
+//
+//  Mortgage.m
+//  Home Mortgage Calculator
+//
+//  Created by Lambda_School_Loaner_241 on 10/13/20.
+//
+
+#import "Mortgage.h"
+
+@implementation Mortgage
+
+@end
---
+++ Home Mortgage Calculator/Home Mortgage CalculatorTests/Home_Mortgage_CalculatorTests.swift
@@ -0,0 +1,33 @@
+//
+//  Home_Mortgage_CalculatorTests.swift
+//  Home Mortgage CalculatorTests
+//
+//  Created by Lambda_School_Loaner_241 on 10/13/20.
+//
+
+import XCTest
+@testable import Home_Mortgage_Calculator
+
+class Home_Mortgage_CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
