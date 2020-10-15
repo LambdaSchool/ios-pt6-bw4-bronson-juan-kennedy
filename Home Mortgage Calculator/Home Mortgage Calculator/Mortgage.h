e5c0dd49dcfe9cf88ffa4561c1922eb10afd281b
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-10-14 18:08:47 -0700

    Starter project, storyboard and basic files

---
+++ Home Mortgage Calculator/Home Mortgage Calculator/Mortgage.h
@@ -0,0 +1,16 @@
+//
+//  Mortgage.h
+//  Home Mortgage Calculator
+//
+//  Created by Lambda_School_Loaner_241 on 10/13/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Mortgage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Home Mortgage Calculator/Home Mortgage Calculator/MortgageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MortgageViewController.swift
+//  Home Mortgage Calculator
+//
+//  Created by Lambda_School_Loaner_241 on 10/13/20.
+//
+
+import UIKit
+
+class MortgageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
