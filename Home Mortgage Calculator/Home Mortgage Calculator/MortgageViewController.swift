723694d0e6e5c182d5551d66f67df661c0164a08
Bronson Mullens <bronsonmullens@icloud.com>
2020-10-17 00:49:07 -0700

    Implemented payment reminders using EventKit. Default due date is the first of the month (will need to be updated to allow user to pick a date). Info.plist was updated to ask user for permission.

--- Home Mortgage Calculator/Home Mortgage Calculator/MortgageViewController.swift
+++ Home Mortgage Calculator/Home Mortgage Calculator/MortgageViewController.swift
@@ -47,7 +47,47 @@ class MortgageViewController: UIViewController {
     }
     
     @IBAction func remindMeButtonTapped(_ sender: UIButton) {
-        // Do EventKit logic here
+        let preReminderAlert = UIAlertController(title: "Set a reminder?",
+                                                 message: "You will be reminded to pay your mortgage at the beginning of each month.",
+                                                 preferredStyle: .alert)
+        let confirmReminderAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let eventStore: EKEventStore = EKEventStore()
+            
+            eventStore.requestAccess(to: .reminder) { (granted, error) in
+                if (granted) && (error == nil) {
+                    print("Permission to access reminders is granted")
+                    
+                    let reminder: EKReminder = EKReminder(eventStore: eventStore)
+                    reminder.title = "Pay mortgage"
+                    // reminder.notes = ""
+                    reminder.calendar = eventStore.defaultCalendarForNewReminders()
+                    reminder.addRecurrenceRule(EKRecurrenceRule(recurrenceWith: .monthly, interval: 1, end: nil))
+                    
+                    guard let interval = Calendar.current.dateInterval(of: .month, for: Date()) else { return }
+                    let lastOfMonth = interval.end
+                    
+                    reminder.dueDateComponents = Calendar.current.dateComponents([.year, .month, .day], from: lastOfMonth)
+                    
+                    do {
+                        try eventStore.save(reminder, commit: true)
+                    } catch let error as NSError {
+                        print("Could not save reminder to event store: \(error)")
+                    }
+                } else {
+                    print("Access was not granted")
+                }
+                
+            }
+            
+            let alert = UIAlertController(title: "Reminder created", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        preReminderAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        preReminderAlert.addAction(confirmReminderAction)
+        
+        self.present(preReminderAlert, animated: true, completion: nil)
     }
     
 
