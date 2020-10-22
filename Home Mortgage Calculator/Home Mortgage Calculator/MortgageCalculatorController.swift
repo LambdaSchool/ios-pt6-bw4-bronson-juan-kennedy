42c072c8452a4a5fc2ec0503b292b656bbf7cb59
Juan M Mariscal <j.m.mariscal03@gmail.com>
2020-10-21 20:34:32 -0700

    Updated Labels, got rid of 2 UISliders

--- Home Mortgage Calculator/Home Mortgage Calculator/MortgageCalculatorController.swift
+++ Home Mortgage Calculator/Home Mortgage Calculator/MortgageCalculatorController.swift
@@ -12,7 +12,6 @@ class MortgageCalculatorController {
     static func calculatePayment(homePrice: Double?, downPayment: Double?, interestRate: Double?, loanDuration: Int?, yearlyPropertyTax: Double?) -> Double {
         
         let homePriceAfterDownPayment = (homePrice ?? 100000.0) - (downPayment ?? 0.0)
-        print("Home Price: \(homePriceAfterDownPayment)")
         let priceWithInterestRate = (homePriceAfterDownPayment * (interestRate ?? 0.0)) + homePriceAfterDownPayment
         let yearlyPayment = priceWithInterestRate / Double(loanDuration ?? 30)
         let totalMonthlyPayment = (yearlyPayment + (yearlyPropertyTax ?? 0.0)) / 12
